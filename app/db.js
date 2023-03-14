56a7ab0027cdef311a9d9bd132c4260188a7540d
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-03-14 19:09:40 +0200

    .env file

--- app/db.js
+++ app/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const mysqlConfigBase = {
-    host: "127.0.0.1",
-    user: "root",
-    password: "r1122m0302",
-    port: 3306
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT
 };
 
 const tasksManagerConnection = mysql.createConnection({
